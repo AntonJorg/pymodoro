636f5b25d7d7221314a5423f53b1a4baa1a005d9
AntonJorg <anton2410@gmail.com>
2021-03-06 22:07:43 +0100

    New icons and session start message

--- main.py
+++ main.py
@@ -15,50 +15,56 @@ import os
 
 class Pomodoro:
     
-    def __init__(self, name, worktime = 25, breaktime = 5, rounds = None):
+    def __init__(self, name, worktime=25, breaktime=5, rounds=None):
         notify2.init(name)    
         self.worktime = worktime
         self.breaktime = breaktime
         self.rounds = rounds
         
     def main_loop(self):
-        
+        n = notify2.Notification("Session started",
+                                 f"{self.rounds} rounds of {self.worktime} minute work sessions and {self.breaktime} minutes breaks."
+                                 )
+        n.show()
+
+        time.sleep(10)
+
         while self.rounds > 0 or self.rounds is None:
             self.work_notification()
             time.sleep(self.worktime * 60)
+            if self.rounds == 1:
+                break
             self.break_notification()
             time.sleep(self.breaktime * 60)
-            
-            self.rounds -= 1
-        
+
+            if self.rounds is not None:
+                self.rounds -= 1
         
     def work_notification(self):
         
         now = datetime.datetime.now()
-        done = now + timedelta(minutes = self.worktime)
+        done = now + timedelta(minutes=self.worktime)
         d = done.strftime("%H:%M")
         
         n = notify2.Notification("Work",
                                  f"{self.worktime} minutes until {d}",
-                                 "notification-message-im"   # Icon name
-                                )
+                                 os.path.join(os.getcwd(), "work.png")  # Icon name
+                                 )
         n.show()
     
     def break_notification(self):
         
         now = datetime.datetime.now()
-        done = now + timedelta(minutes = self.breaktime)
+        done = now + timedelta(minutes=self.breaktime)
         d = done.strftime("%H:%M")
         
         n = notify2.Notification("Break",
                                  f"{self.breaktime} minutes until {d}",
-                                 os.path.join(os.getcwd(), "break.png") # Icon name
-                                )
+                                 os.path.join(os.getcwd(), "break.png")  # Icon name
+                                 )
         n.show()
-        
+
 
 if __name__ == "__main__":    
-    timer = Pomodoro("Pymodoro", 0.1, 0.05, 5)
+    timer = Pomodoro("Pymodoro", 0.5, 0.25, 3)
     timer.main_loop()
-
-    
